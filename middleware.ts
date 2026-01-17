0476b637345eba75488be5dda84fc2bd8c2a3fe2
Michael <usalandspecialist@gmail.com>
2026-01-17 17:14:26 -0600

    Remove middleware.ts to satisfy Netlify edge bundling

--- middleware.ts
+++
@@ -1,5 +0,0 @@
-// NOTE:
-// We cannot use Next.js middleware in this repo because production builds can fail resolving
-// internal `next/server` exports. We gate the UI in `app/layout.tsx` instead.
-export {};
-
